ae999d997d123ece028319ccd3de256d815f9473
Paulina Lubasińska <lubasinska.paulina@gmail.com>
2022-04-01 08:28:53 +0200

    add gulp basic gulp settings, add scss structure

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask(){
-  return src('app/scss/style.scss', { sourcemaps: true })
+  return src('app/scss/index.scss', { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
     .pipe(dest('dist', { sourcemaps: '.' }));
@@ -15,7 +15,7 @@ function scssTask(){
 
 // JavaScript Task
 function jsTask(){
-  return src('app/js/script.js', { sourcemaps: true })
+  return src('app/js/index.js', { sourcemaps: true })
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,4 @@
-# Gulp & BrowserSync setup
-
-Source code for the Browsersync + Gulp tutorial on Coder Coder.
-
-Check out the full tutorials on [YouTube](https://youtu.be/q0E1hbcj-NI) and on [my blog](https://coder-coder.com/quick-guide-to-browsersync-gulp-4/)!
+### Boilerplate created by Paulina Lubasińska 
+## with friends from youtube
+# Coder Coder (https://www.youtube.com/channel/UCzNf0liwUzMN6_pixbQlMhQ)
 
